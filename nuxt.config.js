4785799fa1f77958ef63884d5a772ded504e79e8
Noppawit Thairungroj <noppawit.trr@gmail.com>
2021-03-30 18:35:51 +0700

    add pam4-tracker package

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,7 +72,7 @@ module.exports = {
     /*
      ** Plugins to load before mounting the App
      */
-    plugins: [],
+    plugins: ['~/plugins/pam'],
 
     /*
      ** Nuxt.js modules
---
+++ plugins/pam.ts
@@ -0,0 +1,28 @@
+import Pam, { IConfig } from 'pam4-tracker'
+
+export default ({ app }, inject) => {
+  // TODO: fill these configs
+  const config: IConfig = {
+    autoTracking: false,
+    webPushPublicKey: '',
+    baseApi: '',
+    pusherAlias: '',
+    consentMessageId: '',
+    publicDBAlias: '',
+    loginDBAlias: '',
+  }
+
+  const pam = new Pam(config)
+
+  inject('pam', pam)
+
+  app.router.afterEach(() => {
+    pam.send('page_view', '', {})
+      .then((res) => {
+        // response
+      })
+      .catch((err) => {
+        // handle error
+      })
+  })
+}
