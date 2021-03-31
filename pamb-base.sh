9ef41bfe094a67c079b33d0a5a9e7e26f125049c
Piyachet Rungsrisuwakul <piyachet@3dsinteractive.com>
2021-03-31 18:40:43 +0700

    path

--- pamb-base.sh
+++ pamb-base.sh
@@ -7,7 +7,7 @@ function commit() {
 }
 
 function build_demoecommerce() {
-    local IMAGE=3dsinteractive/pam4-demo:
-    docker build -t $IMAGE$TIMESTAMP .
+    local IMAGE=3dsinteractive/pam4-demo:pam-demo-web
+    docker build -t $IMAGE.$TIMESTAMP .
     commit $IMAGE
 }
