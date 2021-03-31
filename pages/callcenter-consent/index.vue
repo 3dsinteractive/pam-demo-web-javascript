45ad1afa7d6514bce9c96d03ae1f5559786c325b
Jia <supawit@3dsinteractive.com>
2021-03-31 16:29:30 +0700

    Added empty callcenter-consent page

---
+++ pages/callcenter-consent/index.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+	name: 'callcenter-consent',
+  
+  data () {
+    return {
+    };
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
