e412796e6f0bceee01fcadb3ed9a7dc95e0da6f2
Jia <supawit@3dsinteractive.com>
2021-04-06 09:25:32 +0700

    Added consent edit feature on menu dropdown

--- components/menu/Menu.vue
+++ components/menu/Menu.vue
@@ -1,34 +1,75 @@
 <template>
 	<div class="user-menu-container">
-		<div class="navbar-item">
-			<div class="field is-grouped">
-				<p class="control user-menu" v-if="!isUserLoggedIn" @click="toSignupPage">
-					<!-- <a v-if="!isUserLoggedIn" @click="toSignupPage"> -->
-						<!-- <span class="icon">
-							<i class="fa fa-user-plus"></i>
-						</span> -->
-						<span>{{ signupLabel }}</span>
-					<!-- </a> -->
-				</p>
-				<p class="control user-menu" v-if="!isUserLoggedIn" @click="toLoginPage">
-					<!-- <a v-if="!isUserLoggedIn" @click="toLoginPage"> -->
-						<!-- <span class="icon">
-							<i class="fa fa-user"></i>
-						</span> -->
-						<span>{{ loginLabel }}</span>
-					<!-- </a> -->
-				</p>
+		<div class="navbar-item has-dropdown is-hoverable user-menu">
+			<a class="navbar-link">
+				<span class="icon">
+					<i class="fa fa-user"></i>
+				</span>
+			</a>
+			
+		  <div class="navbar-dropdown">
+        <a class="navbar-item" v-if="!isUserLoggedIn" @click="toSignupPage">
+					{{ signupLabel }}
+        </a>
+				<a class="navbar-item" v-if="!isUserLoggedIn" @click="toLoginPage">
+					{{ loginLabel }}
+				</a>
+				<hr class="navbar-divider">
+				<div class="nested navbar-item dropdown">
+          <div class="dropdown-trigger">
+            <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+							<span class="icon is-small">
+								<i class="fa fa-angle-left" aria-hidden="true"></i>
+							</span>
+							<span>Consent Setting</span>
+						</button>
+          </div>
+					
+					<div class="dropdown-menu" id="dropdown-menu" role="menu">
+						<div class="dropdown-content">
+							<a v-for="(consentMsgData,id) in consentData" :key="id" @click="() => popupConsentModal(consentMsgID)" class="dropdown-item">
+								{{consentMsgData.name}}
+							</a>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
+
+		<!-- <div class="navbar-item">
+			<div class="field is-grouped">
+				<p class="control user-menu" v-if="!isUserLoggedIn" @click="toSignupPage">
+						<span>{{ signupLabel }}</span>
+				</p>
+				<p class="control user-menu" v-if="!isUserLoggedIn" @click="toLoginPage">
+						<span>{{ loginLabel }}</span>
+				</p>
+			</div>
+		</div> -->
 		<div v-if="isUserLoggedIn" class="navbar-item has-dropdown is-hoverable">
 			<a class="navbar-link">
 				Welcome {{ getUserName }}
 			</a>
 			<div class="navbar-dropdown is-boxed">
-				<!-- <nuxt-link class="navbar-item" :to="{ name: 'user-wishlist' }">
-					{{ wishlistLabel }}
-				</nuxt-link>
-				<hr class="navbar-divider"> -->
+				<div class="nested navbar-item dropdown">
+          <div class="dropdown-trigger">
+            <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+							<span class="icon is-small">
+								<i class="fa fa-angle-left" aria-hidden="true"></i>
+							</span>
+							<span>Consent Setting</span>
+						</button>
+          </div>
+					
+					<div class="dropdown-menu" id="dropdown-menu" role="menu">
+						<div class="dropdown-content">
+							<a v-for="consentMsgID in consentData" :key="consentMsgID" @click="() => popupConsentModal(consentMsgID)" class="dropdown-item">
+								{{consentData[consentMsgID].name}}
+							</a>
+						</div>
+					</div>
+				</div>
+				<hr class="navbar-divider">
 				<a class="navbar-item" @click="logout">
 					{{ logoutLabel }}
 				</a>
@@ -45,9 +86,36 @@ export default {
 			wishlistLabel: 'Wishlist',
 			logoutLabel: 'Log out',
 			loginLabel: 'Log in',
-			signupLabel: 'Sign up'
+			signupLabel: 'Sign up',
+			consentData: {
+        '1qHkMGcSLHcJxyqZXRq6ovIM8GV': {
+          name: 'Consent for Tracking',
+          submitBody: {},
+					initData: {},
+        },
+        '1qZBgGUvpBI4heCPQzfocNyuY1D': {
+          name: 'Consent for Contacting 1',
+          submitBody: {},
+					initData: {},
+        },
+        '1qZCeSoXiawAYwTz5mmop5YyJWf': {
+          name: 'Consent for Contacting 2',
+          submitBody: {},
+					initData: {},
+        }
+      },
+			popUpCollection: {},
 		}
 	},
+	
+  async mounted() {
+    for (let id in this.consentData) {
+			await this.$pam.consentManager.getCustomerConsentDetail(id).then((msgData) => {this.consentData[id].initData = msgData});
+      await this.$pam.consentManager.createPopup(id,true,this.sdkCallback).then((popUp) => {this.popUpCollection[id] = popUp});
+      await this.popUpCollection[id].renderOnlyPopup();
+      // await this.popUpCollection[id].unAcceptAllConsent();
+    }
+  },
 
 	computed: {
 		isUserLoggedIn () {
@@ -75,7 +143,11 @@ export default {
 		toSignupPage () {
 			// this.$store.commit('toSignupPage', true);
 			this.$router.push({ name: 'signup' });
-		}
+		},
+    popupConsentModal(consentMsgID) {
+      // this.onChecking = false
+      this.popUpCollection[consentMsgID].renderOnlyPopup();
+    },
 	}
 }
 </script>
@@ -96,6 +168,27 @@ export default {
 		cursor: pointer;
 		color: #FFFFFF;
 		margin: 0 15px;
+	}
+	.user-menu>.navbar-item {
+		text-align: right;
+	}
+	.nested.dropdown {
+		&:hover > .dropdown-menu {
+			display: block;
+		}
+		.dropdown-menu {
+			top: -11px;
+			margin-left: -118%;
+		}
+		.dropdown-trigger {
+			button {
+				padding: 0px 0px;
+				border: 0px;
+				font-size: 14px;
+				font-weight: 400;
+				height: 2em;
+			}
+		}
 	}
 </style>
 
